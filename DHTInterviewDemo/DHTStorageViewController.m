f1f8ed3a493ca4f184f77fb55df2851553591903
happyo <xiaxiongzhi@duotin.com>
2016-01-19 15:20:14 +0800

    feat(数据持久化)：新增 SQLite 相关知识

--- DHTInterviewDemo/DHTStorageViewController.m
+++ DHTInterviewDemo/DHTStorageViewController.m
@@ -8,9 +8,12 @@
 
 #import "DHTStorageViewController.h"
 #import "Car.h"
+#import <sqlite3.h>
 
 @interface DHTStorageViewController ()
-
+{
+    sqlite3 *myDb;
+}
 @end
 
 @implementation DHTStorageViewController
@@ -23,7 +26,9 @@
     
 //    [self writeWithPreferences];
     
-    [self writeWithArchiver];
+//    [self writeWithArchiver];
+    
+    [self writeWIthSQLite];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -32,6 +37,76 @@
 }
 
 #pragma mark -- Private Methods --
+
+- (void)writeWIthSQLite
+{
+    // 获取文件路径名
+    NSString *fileName = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).firstObject stringByAppendingString:@"person.sqlite"];
+    
+    // 打开数据库
+    NSInteger openResult = sqlite3_open(fileName.UTF8String, &myDb);
+    if (openResult == SQLITE_OK) {
+        NSLog(@"打开数据库成功");
+        
+        // 创建表 SQL
+        NSString *sqlCreateTable = @"CREATE TABLE IF NOT EXISTS person_info (ID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INTEGER)";
+        char *errmsg = NULL;
+        
+        // 执行语句
+        sqlite3_exec(myDb, sqlCreateTable.UTF8String, NULL, NULL, &errmsg);
+        
+        if (errmsg) {
+            NSLog(@"建表失败： %s", errmsg);
+            return;
+        } else {
+            NSLog(@"建表成功");
+        }
+        
+        
+        // 插入10条数据
+        for (int i = 1; i < 10; i++) {
+            NSString *nameStr = [NSString stringWithFormat:@"happyo%i", i];
+            NSInteger age = i;
+            NSString *sqlInsertData = [NSString stringWithFormat:@"INSERT INTO person_info (name, age) VALUES ('%@', '%ld')",nameStr , age];
+            
+            // 执行语句
+            sqlite3_exec(myDb, sqlInsertData.UTF8String, NULL, NULL, &errmsg);
+            
+            if (errmsg) {
+                NSLog(@"插入失败： %s", errmsg);
+                return;
+            }
+        }
+        
+        NSLog(@"插入成功");
+        
+        
+        // 查询数据
+        NSString *sqlQuery = @"SELECT name, age FROM person_info";
+        sqlite3_stmt *stmt;
+        
+        NSInteger queryResult = sqlite3_prepare_v2(myDb, sqlQuery.UTF8String, -1, &stmt, NULL);
+        if (queryResult == SQLITE_OK) {
+            while (sqlite3_step(stmt) == SQLITE_ROW) {
+                char *name = (char *)sqlite3_column_text(stmt, 0);
+                NSInteger age = sqlite3_column_int(stmt, 1);
+                
+                NSLog(@"name :%s , age :%ld", name, age);
+            }
+        }
+        
+        sqlite3_finalize(stmt);
+        
+    } else {
+        NSLog(@"打开数据库失败");
+        sqlite3_close(myDb);
+        return;
+    }
+    
+
+    
+    sqlite3_close(myDb);
+}
 
 - (void)writeWithArchiver
 {
