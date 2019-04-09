e4331050af830bba3a03de3856125924e5dbf8f1
happyo <happyojones@gmail.com>
2019-04-09 19:00:37 +0800

    add Run Loop

--- DHTInterviewDemo/RunLoop/DHTRunLoopViewController.m
+++ DHTInterviewDemo/RunLoop/DHTRunLoopViewController.m
@@ -16,6 +16,47 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self threadMain];
 }
+
+// Creating a run loop observer
+- (void)threadMain
+{
+    NSRunLoop *myRunLoop = [NSRunLoop currentRunLoop];
+    
+    // Create a run loop observer and attach it to the run loop
+    CFRunLoopObserverContext context = {0, (__bridge void *)(self), NULL, NULL, NULL};
+    CFRunLoopObserverRef observer = CFRunLoopObserverCreate(kCFAllocatorDefault, kCFRunLoopAllActivities, YES, 0, &myRunLoopObserver, &context);
+    
+    if (observer) {
+        CFRunLoopRef cfLoop = [myRunLoop getCFRunLoop];
+        CFRunLoopAddObserver(cfLoop, observer, kCFRunLoopDefaultMode);
+    }
+    
+    // Create and schedule the timer
+    [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(doFireTimer:) userInfo:nil repeats:YES];
+    
+    NSInteger loopCount = 10;
+    
+    do
+    {
+        // Run the run loop 10 times to let the timer fire.
+        [myRunLoop runUntilDate:[NSDate dateWithTimeIntervalSinceNow:1]];
+        loopCount--;
+    }
+    while (loopCount);
+}
+
+- (void)doFireTimer:(NSTimer *)timer
+{
+    NSLog(@"timer");
+}
+
+void myRunLoopObserver(CFRunLoopObserverRef observer, CFRunLoopActivity activity, void *info)
+{
+    NSLog(@"ob");
+}
+
+
 
 @end
